d0db374242e0a9f4a80ac8ff0fec195f189b4114
Giulia834 <giulia.p.mendes@gmail.com>
2024-06-13 13:53:44 -0300

    Emojis

---
+++ Manager/src/EmojiRenderer.java
@@ -0,0 +1,19 @@
+import java.awt.Component;
+
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
+
+public class EmojiRenderer extends DefaultTableCellRenderer {
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value,
+                                                   boolean isSelected, boolean hasFocus, int row, int column) {
+        JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        boolean played = (boolean) value;
+        label.setText(played ? "🎮 ✔️" : "🎮 ❌");
+        label.setHorizontalAlignment(SwingConstants.CENTER); // Centraliza o texto
+        return label;
+    }
+}
